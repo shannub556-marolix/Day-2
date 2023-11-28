6911ebeb253dbb61e61f473d9ad98c49c2b4ae9c
Busappagari Shanmukha <shannub556.marolix@gmail.com>
2023-11-28 12:25:24 +0530

    updated on 28/11/23

--- pratice.py
+++ pratice.py
@@ -1,9 +1,29 @@
-n1=eval(input('enter ypour decimal num'))
-rev=0
-n=1
-while n1!=0:
-    rem=n1%2
-    rev=rev+rem*n
-    n1//=2
-    n*=10
-print(rev)
+'''
+string='praveen is a good boy'
+
+1. 'boy good is a praveen'
+2. 'neevarp si a doog yob'
+3. 'yob doog si a neevarp'
+'''
+s='praveen is a good boy '
+s=s.split()
+print((' ').join(s[::-1]))
+
+
+s='praveen is a good boy '
+s=s.split()
+s1=''
+for ch in s:
+    s1+=ch[::-1]+' '
+print(s1)
+
+
+s='praveen is a good boy '
+s=s.split()
+s=s[::-1]
+s1=''
+for ch in s:
+    s1+=ch[::-1]+' '
+print(s1)
+import keyword
+print(keyword.kwlist)
